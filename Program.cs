abc4e9c8766f11210e7cd2602042f781d43c4a59
iditchev <iditchev@my.wctc.edu>
2021-03-01 18:45:36 -0600

    Adding intial nlog

--- Program.cs
+++ Program.cs
@@ -1,12 +1,21 @@
 ﻿using System;
+using NLog.Web;
+using System.IO;
+using System.Collections.Generic;
 
 namespace MediaLibrary
 {
     class Program
     {
+        //create static instance of logger
+        private static NLog.Logger logger = NLog.Web.NLogBuilder.ConfigureNLog("nlog.config").GetCurrentClassLogger();
         static void Main(string[] args)
         {
+            logger.Info("Program started");
+
             Console.WriteLine("Hello World!");
+
+            logger.Info("Program ended");
         }
     }
 }
