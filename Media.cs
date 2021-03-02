68b1c36a8ab4bc7be094e77540c814368d6152e5
iditchev <iditchev@my.wctc.edu>
2021-03-01 18:57:42 -0600

    Album Class

--- Media.cs
+++ Media.cs
@@ -34,6 +34,16 @@ using System;
          }
      
      }
+  // Album class is derived from Media class
+     public class Album : Media
+     {
+         public string artist { get; set; }
+         public string recordLabel { get; set; }
 
+         public override string Display()
+         {
+             return $"Id: {mediaId}\nTitle: {title}\nArtist: {artist}\nLabel: {recordLabel}\nGenres: {string.Join(", ", genres)}\n";
+         }
+     }
 
  }
