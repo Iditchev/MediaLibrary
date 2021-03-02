e693c728a4a1d9c3dccb4a10f79a5ffcb0bb1d2e
iditchev <iditchev@my.wctc.edu>
2021-03-01 20:32:28 -0600

    Add display file

--- Program.cs
+++ Program.cs
@@ -11,6 +11,9 @@ namespace MediaLibrary
         private static NLog.Logger logger = NLog.Web.NLogBuilder.ConfigureNLog("nlog.config").GetCurrentClassLogger();
         static void Main(string[] args)
         {
+              string scrubbedFile = FileScrubber.ScrubMovies("movies.csv");
+             logger.Info(scrubbedFile);
+
             string movieFilePath = Directory.GetCurrentDirectory() + "\\movies.csv";
             logger.Info("Program started");
 
@@ -67,42 +70,18 @@ namespace MediaLibrary
 
                         movieFile.AddMovie(movie);
                     }
+       } else if (choice == "2")
+                {
+                    // Display All Movies
+                    foreach(Movie m in movieFile.Movies)
+                    {
+                        Console.WriteLine(m.Display());
+                    }
+                }
+            } while (choice == "1" || choice == "2");
+              
 
-              // Movie movie = new Movie
-             // {
-             //     mediaId = 123,
-             //     title = "Greatest Movie Ever, The (2020)",
-             //     director = "Jeff Grissom",
-             //     // timespan (hours, minutes, seconds)
-             //     runningTime = new TimeSpan(2, 21, 23),
-             //     genres = { "Comedy", "Romance" }
-             // };
-
-             // Console.WriteLine(movie.Display());
-
-             // Album album = new Album
-             // {
-             //     mediaId = 321,
-             //     title = "Greatest Album Ever, The (2020)",
-             //     artist = "Jeff's Awesome Band",
-             //     recordLabel = "Universal Music Group",
-             //     genres = { "Rock" }
-             // };
-             // Console.WriteLine(album.Display());
-
-             // Book book = new Book
-             // {
-             //     mediaId = 111,
-             //     title = "Super Cool Book",
-             //     author = "Jeff Grissom",
-             //     pageCount = 101,
-             //     publisher = "",
-             //     genres = { "Suspense", "Mystery" }
-             // };
-             // Console.WriteLine(book.Display());
-
-             string scrubbedFile = FileScrubber.ScrubMovies("movies.csv");
-             logger.Info(scrubbedFile);
+           
 
             logger.Info("Program ended");
         }
